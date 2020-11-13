68cec45261e517fb33a8075a23371e2d0a3d3577
yolotan12345 <72150613+yolotan12345@users.noreply.github.com>
2020-11-13 17:29:45 +0800

    Added door sensor for URATTinyBle

--- sensor/UARTTinyBle/UARTTinyBle.ino
+++ sensor/UARTTinyBle/UARTTinyBle.ino
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------
 //  TinyCircuits ST BLE TinyShield UART Example Sketch
-//  Last Updated 2 March 2016 
+//  Last Updated 2 March 2016
 //
 //  Reference from Ben Rose, TinyCircuits http://tinycircuits.com
 //-------------------------------------------------------------------------------
@@ -26,31 +26,59 @@ uint8_t ble_rx_buffer_len = 0;
 uint8_t ble_connection_state = false;
 #define PIPE_UART_OVER_BTLE_UART_TX_TX 0
 
+int door_pin = 3;
+
 void setup() {
   Serial.begin(9600);
   SerialMonitorInterface.begin(9600);
+  pinMode(door_pin, INPUT);
+  digitalWrite(door_pin, HIGH);
+
   //while (!SerialMonitorInterface); //This line will block until a serial monitor is opened with TinyScreen+!
   BLEsetup();
 }
 
 void loop() {
   aci_loop();//Process any ACI commands or events from the NRF8001- main BLE handler, must run often. Keep main loop short.
-    if(ble_connection_state == 1){
+  if (ble_connection_state == 1) {
     SerialMonitorInterface.print("connected \n");
 
-    char* word = "hello";
-    int word_len = 0;  
-    for (int i = 0; word[i] != '\0'; ++i) {
-      ++word_len;
+    if (!digitalRead(door_pin))
+    {
+      char* word = "0x02";
+      // u_int8_t word = 0x68;
+
+      int word_len = 0;
+      for (int i = 0; word[i] != '\0'; ++i) {
+        ++word_len;
+      }
+      lib_aci_send_data((uint8_t)0, (uint8_t*)word, (uint8_t)word_len);
+      if (!lib_aci_send_data((uint8_t)0, (uint8_t*)word, (uint8_t)word_len))
+      {
+        SerialMonitorInterface.println(F("TX dropped!"));
+      }
     }
-    lib_aci_send_data((uint8_t)0, (uint8_t*)word, (uint8_t)word_len);
+    else
+    {
+      char* word = "0x04";
+      int word_len = 0;
+      for (int i = 0; word[i] != '\0'; ++i) {
+        ++word_len;
+      }
+      lib_aci_send_data((uint8_t)0, (uint8_t*)word, (uint8_t)word_len);
+      if (!lib_aci_send_data((uint8_t)0, (uint8_t*)word, (uint8_t)word_len))
+      {
+        SerialMonitorInterface.println(F("TX dropped!"));
+      }
+    }
     delay(2000);
   }
-  else{
+  else {
     SerialMonitorInterface.print("not connected \n");
     delay(2000);
   }
 
+  //Reciving from the prehipials device
   if (ble_rx_buffer_len) {//Check if data is available
     SerialMonitorInterface.print(ble_rx_buffer_len);
     SerialMonitorInterface.print(" : ");
@@ -59,25 +87,21 @@ void loop() {
 
     ble_rx_buffer_len = 0;//clear afer reading
   }
-  if (SerialMonitorInterface.available()) {//Check if serial input is available to send
-    delay(10);//should catch input
-    uint8_t sendBuffer[21];
-    uint8_t sendLength = 0;
-    while (SerialMonitorInterface.available() && sendLength < 19) {
-      sendBuffer[sendLength] = SerialMonitorInterface.read();
-      sendLength++;
-    }
-    if (SerialMonitorInterface.available()) {
-      SerialMonitorInterface.print(F("Input truncated, dropped: "));
-      if (SerialMonitorInterface.available()) {
-        SerialMonitorInterface.write(SerialMonitorInterface.read());
-      }
-    }
-    sendBuffer[sendLength] = '\0'; //Terminate string
-    sendLength++;
-    if (!lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)sendBuffer, sendLength))
-    {
-      SerialMonitorInterface.println(F("TX dropped!"));
-    }
-  }
+  // Send data from serial monitor
+//  if (SerialMonitorInterface.available()) {//Check if serial input is available to send
+//    delay(10);//should catch input
+//    uint8_t sendBuffer[21];
+//    uint8_t sendLength = 0;
+//    while (SerialMonitorInterface.available() && sendLength < 19) {
+//      sendBuffer[sendLength] = SerialMonitorInterface.read();
+//      sendLength++;
+//    }
+//    if (SerialMonitorInterface.available()) {
+//      SerialMonitorInterface.print(F("Input truncated, dropped: "));
+//      if (SerialMonitorInterface.available()) {
+//        SerialMonitorInterface.write(SerialMonitorInterface.read());
+//      }
+//    }
+//    sendBuffer[sendLength] = '\0'; //Terminate string
+//    sendLength++;
 }
