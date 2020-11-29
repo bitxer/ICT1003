f8d24f2f38ec3d592ee601f660ffa1d1935aad6d
bitxer <24569832+bitxer@users.noreply.github.com>
2020-11-30 00:19:23 +0800

    optimise docker build

--- server/web/docker-entrypoint.sh
+++ server/web/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-ln -sf /usr/share/zoneinfo/$TZ /etc/timezone
-ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-
 echo "[*] Starting HTTP Listener..."
 gunicorn "listener:init_app()" --bind 0.0.0.0:5000 --worker-class "gevent" --workers 3 --preload --access-logfile "/opt/web/logs/access.log" --error-logfile "/opt/web/logs/error.log"
